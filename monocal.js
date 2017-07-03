417afcfab889a789f72183b9d5dfcae3dbbe0282
Josh Avanier <joshavanier@gmail.com>
2017-07-03 18:56:55 +0700

    Clean up code

--- monocal.js
+++ monocal.js
@@ -34,26 +34,12 @@
 
   // TODO: Manage leap years
 
-  function convert(d) {
-    var quarter = getQuarter(d),
-        month = getMonth(d),
-        week = getWeek(d),
-        date = getDate(d),
-        day = getDay(d)
-
-    return [quarter, month, week, date, day]
-  }
-
-  // Not sure if this is even necessary
-  function getDay(n) {
-    var d = (new Date()).getDay()
+  function getDay() {
     return DAY[(new Date()).getDay() - 1]
   }
 
   function getDate(n) {
-    var decimal = Math.floor(n / 28),
-        date = (n + 1) - (28 * decimal)
-    return date
+    return (n + 1) - (28 * Math.floor(n / 28))
   }
 
   function getMonth(n) {
@@ -72,22 +58,22 @@
     return t.substring(0, 3).toUpperCase()
   }
 
-  var res = convert(gY),
-      mQ = res[0],
-      mM = res[1],
-      mW = res[2],
-      mT = res[3],
-      mD = res[4],
+  var mQ = getQuarter(gY), // mono quarter
+      mM = getMonth(gY), // mono month
+      mW = getWeek(gY), // mono week
+      mT = getDate(gY), // mono date
+      mD = getDay(gY), // mono day
 
       year = gD.getFullYear().toString().substr(-2),
 
       shortForm = abbr(mD) + " " + mT + " " + abbr(mM) + " " + year
 
   a.getElementsByTagName("span")[0].innerHTML=shortForm
-
   a.getElementById("quarter").innerHTML=mQ
   a.getElementById("month").innerHTML=mM
   a.getElementById("week").innerHTML=mW
   a.getElementById("day").innerHTML=mD
+
+  a=(new Date).getHours();document.body.className=6>=a||18<=a?"dark":"light"
 
 })(this.document)
