11cfd74c211b20192f0c15bd8066ae4c1fcb9b3c
Josh Avanier <joshavanier@gmail.com>
2017-07-05 12:16:43 +0700

    Clean up code

--- monocal.js
+++ monocal.js
@@ -27,19 +27,19 @@
       gS = new Date(gD.getFullYear(), 0, 0),
       gY = Math.floor((gD - gS) / (1000 * 60 * 60 * 24))
 
-  function toDY()  { return DAY[(new Date()).getDay() - 1] }
+  function toDY()  { return (new Date()).getDay() - 1 }
   function toDT(n) { return (n + 1) - (28 * Math.floor(n / 28)) }
-  function toMN(n) { return MON[Math.ceil(n / 28)] }
+  function toMN(n) { return Math.ceil(n / 28) }
   function toWK(n) { return Math.floor(n / 7) }
-  function toQR(n) { return QUR[(toWK(n) / 13) - 1] }
+  function toQR(n) { return (toWK(n) / 13) - 1 }
   function abbr(t) { return t.substring(0, 3).toUpperCase() }
 
-  var mM = abbr(toMN(gY)),
+  var mM = abbr(MON[toMN(gY)]),
       mT = toDT(gY),
-      mD = abbr(toDY(gY))
+      mD = abbr(DAY[toDY(gY)])
 
   a.getElementsByTagName("span")[0].innerHTML=mD + " " + mT + " " + mM + " " + gD.getFullYear().toString().substr(-2)
-  a.getElementById("q").innerHTML=toQR(gY)
+  a.getElementById("q").innerHTML=QUR[toQR(gY)]
   a.getElementById("m").innerHTML=mM
   a.getElementById("w").innerHTML=toWK(gY)
   a.getElementById("d").innerHTML=mD
