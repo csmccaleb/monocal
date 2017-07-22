93df515cd121e65f60dbb0ecf1c2c9314904f823
Josh Avanier <joshavanier@gmail.com>
2017-07-22 17:17:52 +0700

    Add monocal.py

---
+++ py/monocal.py
@@ -0,0 +1,31 @@
+"""
+MONOCAL in Python
+
+Josh Avanier
+
+Currently under development
+Currently learning Python lol
+"""
+
+import datetime
+
+def day(day):
+    """Returns the day of the week"""
+    return day
+
+def date(date):
+    """Returns the MONOCAL date"""
+    return date
+
+def month(month):
+    """Returns the MONOCAL month"""
+    return month
+
+def week(week):
+    """Returns the week number"""
+    print "It is the %d week." % (week)
+    return week
+
+def quarter(quarter):
+    """Returns the quarter"""
+    return quarter
