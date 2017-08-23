32d73e0e24f275868d4239b1a67be0b2cb7ccf9b
Josh Avanier <joshavanier@gmail.com>
2017-08-23 18:48:44 +0700

    Simplify Switch to array

--- sh/monocal.sh
+++ sh/monocal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+M=(UNUM DUOM TRES QUATTR QUINT SEX SEPT OCT NON UNDEC DUDEC TREDEC)
+
 Y=$(date +"%y")
 N=$(date +"%j")
 F=$(($N/28))
@@ -16,18 +18,4 @@ if [ $(($N%28)) -gt 0 ]; then
   C=$(($C + 1))
 fi
 
-if [ $C = 1 ]; then M="UNUM"
-elif [ $C = 2 ]; then M="DUOM"
-elif [ $C = 3 ]; then M="TRES"
-elif [ $C = 4 ]; then M="QUATTR"
-elif [ $C = 5 ]; then M="QUINT"
-elif [ $C = 6 ]; then M="SEX"
-elif [ $C = 7 ]; then M="SEPT"
-elif [ $C = 8 ]; then M="OCT"
-elif [ $C = 9 ]; then M="NON"
-elif [ $C = 10 ]; then M="UNDEC"
-elif [ $C = 11 ]; then M="DUDEC"
-elif [ $C = 12 ]; then M="TREDEC"
-fi
-
-echo "$D $M $Y"
+echo "$D ${M[$C - 1]} $Y"
