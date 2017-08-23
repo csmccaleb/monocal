e5a5b8d560b4b413e2f32391753dfb677a7be9a4
Josh Avanier <joshavanier@gmail.com>
2017-08-23 18:28:19 +0700

    Clean

--- sh/monocal.sh
+++ sh/monocal.sh
@@ -7,52 +7,34 @@
 #
 # MIT
 
-YEAR=$(date +"%y")
-NTH=$(date +"%j")
+Y=$(date +"%y")
+N=$(date +"%j")
+F=$(($N/28))
 
-# calculate date
-
-FLOOR=$(($NTH/28))
-
-if [ $(($NTH%28)) -lt 0 ]; then
-  FLOOR=$((FLOOR - 1))
+if [ $(($N%28)) -lt 0 ]; then
+  F=$((F - 1))
 fi
 
-MULTIPLIER=$(expr 28 \* $FLOOR)
-DATE=$(expr $NTH - $MULTIPLIER - 1)
+X=$(expr 28 \* $F)
+MD=$(expr $N - $X - 1)
+C=$(($N/28))
 
-# calculate month
-
-CEIL=$(($NTH/28))
-
-if [ $(($NTH%28)) -gt 0 ]; then
-  CEIL=$(($CEIL + 1))
+if [ $(($N%28)) -gt 0 ]; then
+  C=$(($C + 1))
 fi
 
-if [ $CEIL = 1 ]; then
-  MONTH="UNUM"
-elif [ $CEIL = 2 ]; then
-  MONTH="DUOM"
-elif [ $CEIL = 3 ]; then
-  MONTH="TRES"
-elif [ $CEIL = 4 ]; then
-  MONTH="QUATTR"
-elif [ $CEIL = 5 ]; then
-  MONTH="QUINT"
-elif [ $CEIL = 6 ]; then
-  MONTH="SEX"
-elif [ $CEIL = 7 ]; then
-  MONTH="SEPT"
-elif [ $CEIL = 8 ]; then
-  MONTH="OCT"
-elif [ $CEIL = 9 ]; then
-  MONTH="NON"
-elif [ $CEIL = 10 ]; then
-  MONTH="UNDEC"
-elif [ $CEIL = 11 ]; then
-  MONTH="DUDEC"
-elif [ $CEIL = 12 ]; then
-  MONTH="TREDEC"
+if [ $C = 1 ]; then MM="UNUM"
+elif [ $C = 2 ]; then MM="DUOM"
+elif [ $C = 3 ]; then MM="TRES"
+elif [ $C = 4 ]; then MM="QUATTR"
+elif [ $C = 5 ]; then MM="QUINT"
+elif [ $C = 6 ]; then MM="SEX"
+elif [ $C = 7 ]; then MM="SEPT"
+elif [ $C = 8 ]; then MM="OCT"
+elif [ $C = 9 ]; then MM="NON"
+elif [ $C = 10 ]; then MM="UNDEC"
+elif [ $C = 11 ]; then MM="DUDEC"
+elif [ $C = 12 ]; then MM="TREDEC"
 fi
 
-echo "$DATE $MONTH $YEAR"
+echo "$MD $MM $Y"
