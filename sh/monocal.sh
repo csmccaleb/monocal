947f3821a89bfbc43a837abfd726285ff4e09619
Josh Avanier <joshavanier@gmail.com>
2017-08-23 18:36:31 +0700

    Clean

--- sh/monocal.sh
+++ sh/monocal.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# MONOCAL.sh
-# super-rudimentary implementation of MONOCAL in bash
-#
-# Josh Avanier
-#
-# MIT
-
 Y=$(date +"%y")
 N=$(date +"%j")
 F=$(($N/28))
@@ -16,25 +9,25 @@ if [ $(($N%28)) -lt 0 ]; then
 fi
 
 X=$(expr 28 \* $F)
-MD=$(expr $N - $X - 1)
+D=$(expr $N - $X - 1)
 C=$(($N/28))
 
 if [ $(($N%28)) -gt 0 ]; then
   C=$(($C + 1))
 fi
 
-if [ $C = 1 ]; then MM="UNUM"
-elif [ $C = 2 ]; then MM="DUOM"
-elif [ $C = 3 ]; then MM="TRES"
-elif [ $C = 4 ]; then MM="QUATTR"
-elif [ $C = 5 ]; then MM="QUINT"
-elif [ $C = 6 ]; then MM="SEX"
-elif [ $C = 7 ]; then MM="SEPT"
-elif [ $C = 8 ]; then MM="OCT"
-elif [ $C = 9 ]; then MM="NON"
-elif [ $C = 10 ]; then MM="UNDEC"
-elif [ $C = 11 ]; then MM="DUDEC"
-elif [ $C = 12 ]; then MM="TREDEC"
+if [ $C = 1 ]; then M="UNUM"
+elif [ $C = 2 ]; then M="DUOM"
+elif [ $C = 3 ]; then M="TRES"
+elif [ $C = 4 ]; then M="QUATTR"
+elif [ $C = 5 ]; then M="QUINT"
+elif [ $C = 6 ]; then M="SEX"
+elif [ $C = 7 ]; then M="SEPT"
+elif [ $C = 8 ]; then M="OCT"
+elif [ $C = 9 ]; then M="NON"
+elif [ $C = 10 ]; then M="UNDEC"
+elif [ $C = 11 ]; then M="DUDEC"
+elif [ $C = 12 ]; then M="TREDEC"
 fi
 
-echo "$MD $MM $Y"
+echo "$D $M $Y"
