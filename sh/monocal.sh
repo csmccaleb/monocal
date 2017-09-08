e42c4f78c3b4c175ce701fffb366f2dcc8ba6316
Josh Avanier <joshavanier@gmail.com>
2017-09-08 21:50:55 +0700

    Simplify

--- sh/monocal.sh
+++ sh/monocal.sh
@@ -2,7 +2,6 @@
 
 M=(UNUM DUOM TRES QUATTR QUINT SEX SEPT OCT NON UNDEC DUDEC TREDEC)
 
-Y=$(date +"%y")
 N=$(date +"%j")
 F=$(($N/28))
 
@@ -10,12 +9,10 @@ if [ $(($N%28)) -lt 0 ]; then
   F=$(($F - 1))
 fi
 
-X=$(expr 28 \* $F)
-D=$(expr $N - $X - 1)
 C=$(($N/28))
 
 if [ $(($N%28)) -gt 0 ]; then
   C=$(($C + 1))
 fi
 
-echo "$D ${M[$C - 1]} $Y"
+echo "$(($N - (28 * $F) - 1)) ${M[$C - 1]} $(date +"%y")"
