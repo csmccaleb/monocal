d89c14469766777f633fac68ef490948a71ff93b
Josh Avanier <joshavanier@gmail.com>
2017-09-16 14:00:05 +0700

    Clean

--- sh/monocal.sh
+++ sh/monocal.sh
@@ -8,10 +8,7 @@ P=$(($N%28))
 F=$S
 C=$S
 
-if [ $P -lt 0 ]; then F=$(($F-1))
-fi
-
-if [ $P -gt 0 ]; then C=$(($C+1))
-fi
+if [ $P -lt 0 ]; then F=$(($F-1)); fi
+if [ $P -gt 0 ]; then C=$(($C+1)); fi
 
 echo "$(($N-(28*$F)-1)) ${M[$C-1]} $(date +"%y")"
