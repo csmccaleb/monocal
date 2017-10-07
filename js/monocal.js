dc3290d1d1aa8a5a5ba867deceeae20bc3b041a1
Josh Avanier <joshavanier@gmail.com>
2017-10-07 18:16:55 +0700

    ES6

--- js/monocal.js
+++ js/monocal.js
@@ -12,8 +12,8 @@
 
 const MONO = {
 
-  MIY: 13, // months in year
-  DIM: 28, // days in month
+  MIY: 13,
+  DIM: 28,
 
   YDY: "Chomsky Day",
   LDY: "Leap Day",
@@ -24,7 +24,7 @@ const MONO = {
    * @return {string} a full-form date
    */
 
-  full: function(m) {
+  full(m) {
     return m.dt == this.YDY || m.dt == this.LDY ? this.space([m.dt, m.yr]) : this.space([this.pad(m.dt), m.mn, m.yr])
   },
 
@@ -34,7 +34,7 @@ const MONO = {
    * @return {string} a short-form date
    */
 
-  short: function(m) {
+  short(m) {
     let y = m.yr.toString().substr(-2)
     return m.dt == this.YDY || m.dt == this.LDY ? this.space([m.dt, y]) : this.space([this.pad(m.dt), this.abbr(m.mn), y])
   },
@@ -45,7 +45,7 @@ const MONO = {
    * @return {string} a shorter-form date
    */
 
-  shorter: function(m) {
+  shorter(m) {
     let y = m.yr.toString().substr(-2)
     return m.dt == this.YDY || m.dt == this.LDY ? m.dt + y : this.pad(m.dt) + this.abbr(m.mn) + y
   },
@@ -56,7 +56,7 @@ const MONO = {
    * @return {string} a standard-form date
    */
 
-  standard: function(m) {
+  standard(m) {
     return m.dt == this.YDY || m.dt == this.LDY ? this.space([m.dt, m.yr]) : this.space([m.mn, (m.dt + (['st', 'nd', 'rd'][(m.dt + '').match(/1?\d\b/) - 1] || 'th')), m.yr])
   },
 
@@ -66,9 +66,7 @@ const MONO = {
    * @return {object} a MONOCAL object
    */
 
-  convert: function(n) {
-    n = n || new Date()
-
+  convert(n = new Date()) {
     let yer = n.getFullYear(),
         nth = this.nth(n),
         dat = 0,
@@ -115,8 +113,7 @@ const MONO = {
    * @return {number} nth day of the year
    */
 
-  nth: function(d) {
-    d = d || new Date()
+  nth(d = new Date()) {
     return Math.floor((d - new Date(d.getFullYear(), 0, 1)) / 86400000)
   },
 
@@ -125,7 +122,7 @@ const MONO = {
    * @return {string} day of the week
    */
 
-  day: function() {
+  day() {
     return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][(new Date()).getDay()]
   },
 
@@ -135,9 +132,8 @@ const MONO = {
    * @return {number} MONOCAL date
    */
 
-  dat: function(n) {
-    n = n || this.nth((new Date()))
-    let d = n - (this.DIM * Math.floor(n / this.DIM))
+  dat(n = this.nth(new Date())) {
+    let d = n - this.DIM * Math.floor(n / this.DIM)
     return d == 0 ? 28 : d
   },
 
@@ -147,8 +143,7 @@ const MONO = {
    * @return {number} week number
    */
 
-  wek: function(n) {
-    n = n || this.nth((new Date()))
+  wek(n = this.nth(new Date())) {
     return Math.floor(n / 7)
   },
 
@@ -158,8 +153,7 @@ const MONO = {
    * @return {string} MONOCAL month
    */
 
-  mon: function(n) {
-    n = n || this.nth((new Date()))
+  mon(n = this.nth(new Date())) {
     return ["Unumium", "Duomium", "Tresium", "Quattrium", "Quintium", "Sexium", "Septium", "Octium", "Nonium", "Decium", "Undecium", "Dudecium", "Tredecium"][Math.ceil(n / this.DIM) - 1]
   },
 
@@ -169,8 +163,7 @@ const MONO = {
    * @return {string} MONOCAL quarter
    */
 
-  qua: function(n) {
-    n = n || this.nth((new Date()))
+  qua(n = this.nth(new Date())) {
     return ["i.", "ii.", "iii.", "iv."][Math.floor(this.wek(n) / this.MIY)]
   },
 
@@ -180,8 +173,7 @@ const MONO = {
    * @return {string} MONOCAL quarter
    */
 
-  aqu: function(n) {
-    n = n || this.nth((new Date()))
+  aqu(n = this.nth(new Date())) {
     return ["air", "water", "fire", "earth"][Math.floor(this.wek(n) / this.MIY)]
   },
 
@@ -193,9 +185,8 @@ const MONO = {
    * @return {string} array items spaced out
    */
 
-  space: function(a) {
-    let s = ""
-    for (let i = 0, l = a.length; i < l; i++) s += a[i] + " "
+  space(a) {
+    for (var s = "", i = 0, l = a.length; i < l; i++) s += a[i] + " "
     return s.substring(0, s.length - 1)
   },
 
@@ -204,7 +195,7 @@ const MONO = {
    * @return {string} padded number
    */
 
-  pad: function(n) {
+  pad(n) {
     return ('0' + n).substr(-2)
   },
 
@@ -213,7 +204,7 @@ const MONO = {
    * @return {string} MONOCAL month name sans "-ium"
    */
 
-  abbr: function(m) {
+  abbr(m) {
     return m.substring(0, m.length - 3).toUpperCase()
   }
 }
